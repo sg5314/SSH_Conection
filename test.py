abae3c693a1d2e775ba0faeb47403a8599dbc432
SuGa <88835817+sg5314@users.noreply.github.com>
2021-09-04 10:21:43 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from sshtunnel import SSHTunnelForwarder
+import mysql.connector
+
+from sshtunnel import SSHTunnelForwarder
+import mysql.connector
+import pymysql.cursors
+
+port_num = 22
+with SSHTunnelForwarder(
+    ('', port_num),
+    ssh_username='ssugahara',
+    ssh_password='light',
+    ssh_pkey='',
+    remote_bind_address=('', 3306)
+) as server:
+    print(server.local_bind_port)
+    conn = pymysql.connect(
+            host = 'localhost',
+            port = server.local_bind_port,
+            user = '',
+            password = '',
+            database = '',
+            charset = 'utf8',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+    
+    #server='yourserver.database.windows.net', user='yourusername@yourserver', password='yourpassword', database='AdventureWorks'
+
+    cur = conn.cursor()
+
+    print('クエリを実行')
+    # クエリを記述
+    cur.execute("""
+        SELECT * FROM user
+    """)
+
+    for row in cur:
+        print(row)
+
+    cur.close()
+    conn.close()
